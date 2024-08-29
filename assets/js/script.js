c082ade60b1e349d3acaf66e114a43f4358349e6
Parwiz Hussaini <p.syas90@yahoo.com>
2024-08-29 11:13:13 +0000

    Create the startQuiz function and the showQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,3 +156,25 @@ const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next Question";
+    showQuestion();
+}
+
+function showQuestion() {
+    
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        
+    });
+}
