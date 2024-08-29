6cc26b3ef949580d0ea98a339bbb7b107b33d563
Parwiz Hussaini <p.syas90@yahoo.com>
2024-08-29 11:02:12 +0000

    Create variables to store the question index and score

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,3 +153,6 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
