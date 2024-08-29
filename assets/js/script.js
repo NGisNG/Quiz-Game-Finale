2e0ba88c386318f77348a2052eec709228c95210
Parwiz Hussaini <p.syas90@yahoo.com>
2024-08-29 11:41:50 +0000

    Create the selectAnswer function, call this function in the show question function with the help of an EventListiner

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,7 +175,10 @@ function showQuestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
 
@@ -184,6 +187,18 @@ function resetState() {
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else{
+        selectedBtn.classList.add("incorrect");
+    }
+    
 }
 
 startQuiz();
